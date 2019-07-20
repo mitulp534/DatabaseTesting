1f4c03cd45905d9bb727b3be525ea5aafdf37c5c
mitulp534 <mitulp534@gmail.com>
2019-07-20 21:38:20 +0530

    Manifest file updated and content provider added..Coding remaining

---
+++ app/src/main/java/com/example/hp/com/databasetesting/data/PetProvider.java
@@ -0,0 +1,46 @@
+package com.example.hp.com.databasetesting.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class PetProvider extends ContentProvider  {
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    private PetDbHelper mDbHelper;
+    @Override
+    public boolean onCreate() {
+        mDbHelper=new PetDbHelper(getContext());
+        return true;
+    }
+
+
+    @Override
+    public Cursor query( Uri uri,  String[] projection,  String selection,  String[] selectionArgs,  String sortOrder) {
+        return null;
+    }
+
+
+    @Override
+    public String getType( Uri uri) {
+        return null;
+    }
+
+
+    @Override
+    public Uri insert( Uri uri,  ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete( Uri uri,  String selection,  String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update( Uri uri,  ContentValues contentValues,  String selection,  String[] selectionArgs) {
+        return 0;
+    }
+}
